154fc1884543229c0fee290ffc5218d84de034d2
a <a>
2025-12-11 19:22:37 +0100

    v26

--- src/components/DropDownTitle.jsx
+++ src/components/DropDownTitle.jsx
@@ -4,27 +4,32 @@ function DropdownTitle({ title, options }) {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="relative inline-block text-left">
-      {/* Título clickable */}
-      <button onClick={() => setIsOpen(!isOpen)} className="font-bold text-lg">
-        {title} &gt;
-      </button>
+    <article className="w-full justify-start mb-6">
+      <section className="relative inline-block text-left">
+        {/* Título clickable */}
+        <button onClick={() => setIsOpen(!isOpen)} className="font-bold text-4xl dropdownTitle">
+          {title}
+          <span className="ml-2 transition-200ms">
+            {isOpen ? "v" : ">"}
+          </span>
+        </button>
 
-      {/* Opciones desplegables */}
-      {isOpen && (
-        <div className="absolute mt-2 w-48 bg-white border border-gray-300 rounded shadow-lg z-10">
-          {options.map((opt) => (
-            <div
-              key={opt.label}
-              className="px-4 py-2 hover:bg-gray-100 cursor-pointer"
-              onClick={opt.onClick} // acción al hacer click
-            >
-              {opt.label}
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
+        {/* Opciones desplegables */}
+        {isOpen && (
+          <div className="w-full absolute mt-2 bg-white border border-gray-300 rounded shadow-lg z-10">
+            {options.map((opt) => (
+              <div
+                key={opt.label}
+                className="px-4 py-2 hover:bg-gray-100 cursor-pointer"
+                onClick={opt.onClick} // acción al hacer click
+              >
+                {opt.label}
+              </div>
+            ))}
+          </div>
+        )}
+      </section>
+    </article>
   );
 }
 
