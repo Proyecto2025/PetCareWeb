678cc3b371d9c79e036254429c46b50f4619577f
a <a>
2025-12-15 19:20:13 +0100

    v28

--- src/pages/Advice.jsx
+++ src/pages/Advice.jsx
@@ -1,17 +1,37 @@
 import advices from "../data/advice.js";  
+import { useState } from "react";
 import Card from "../components/Card.jsx";  
 import { Link } from "react-router-dom";
+import DropdownTitle from "../components/DropDownTitle.jsx";
 
 function Advice() {
+  const [filtro, setFiltro] = useState("Todo");
+    const filtrado = advices.filter((advice) => 
+        filtro === "Todo" || advice.categoria.toLowerCase() === filtro.toLowerCase()
+    );
+    
+    //Al hacer click se filtre por lo elegido
+    const opciones = [
+    { label: "Todo", onClick: () => setFiltro("Todo") },
+    { label: "Accesorios", onClick: () => setFiltro("Accesorios") },
+    { label: "Comida", onClick: () => setFiltro("Comida") },
+    { label: "Higiene", onClick: () => setFiltro("Higiene") },
+  ];
+
+
   return (
     <>
+    <DropdownTitle
+        title="Consejos"
+        options={opciones}
+      />
       <section
         className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 items-stretch"
       >
         {/*Aqui sería la categoría en vez del tipoAnimal y pongo `/consejo porque o si no, no me encuentra la ruta
           ya que sale /advice/consejo/id pero en app no lo tengo así y por eso no iba
         */}
-        {advices.map((consejos) => ( 
+        {filtrado.map((consejos) => ( 
           <Link
             key={consejos.id}
             to={`/consejo/${consejos.id}`}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,33 @@
 import posts from "../data/post.js";
+import { useState } from "react"; 
 import Card from "../components/Card.jsx";
 import DropdownTitle from "../components/DropDownTitle.jsx";
 
 import { Link } from "react-router-dom";
 
 function Home() {
+  const [filtro, setFiltro] = useState("Todo");
+    const filtrado = posts.filter((post) => 
+        filtro === "Todo" || post.categoria.toLowerCase() === filtro.toLowerCase()
+    );
+    
+    //Al hacer click se filtre por lo elegido
+    const opciones = [
+    { label: "Todo", onClick: () => setFiltro("Todo") },
+    { label: "Adopcion", onClick: () => setFiltro("Adopción") },
+    { label: "Ayuda", onClick: () => setFiltro("Ayuda") },
+    { label: "Extravio", onClick: () => setFiltro("Extravio") },
+  ];
+
+
   return (
     <>
       <DropdownTitle
         title="Publicaciones"
-        options={[
-          { label: "Todo", onClick: () => console.log("Ir a FAQ") },
-          { label: "Adopción", onClick: () => console.log("Ir a FAQ") },
-          { label: "Ayuda", onClick: () => console.log("Ir a contacto") },
-          { label: "Extravio", onClick: () => console.log("Ir a contacto") },
-        ]}
+        options={opciones}
       />
       <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 items-stretch">
-        {posts.map((animales) => (
+        {filtrado.map((animales) => (
           <Link
             key={animales.id}
             to={`/animal/${animales.id}`}
